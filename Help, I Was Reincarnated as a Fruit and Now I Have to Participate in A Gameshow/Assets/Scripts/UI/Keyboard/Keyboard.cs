de924be1a6555d92bdcce71f2f7a4065cd0e3daa
Alfonso <dit19039@uop.gr>
2023-10-23 12:00:18 +0300

    fuckening?

--- Help, I Was Reincarnated as a Fruit and Now I Have to Participate in A Gameshow/Assets/Scripts/UI/Keyboard/Keyboard.cs
+++ Help, I Was Reincarnated as a Fruit and Now I Have to Participate in A Gameshow/Assets/Scripts/UI/Keyboard/Keyboard.cs
@@ -13,12 +13,13 @@ public class Keyboard : MonoBehaviour
     [SerializeField] private KeyboardLine[] lines;
 
     [Header("Key Settings")]
-    [Range(0f, 1f)]
+    [Range(0f, 1)]
     [SerializeField] private float keyToLineRatio;
     [Range(0f, 1f)]
     [SerializeField] private float keyXSpacing;
     [Range(0f, 1f)]
     [SerializeField] private float keyYSpacingRatio = 0.1f;
+<<<<<<< HEAD
 
     [Header("Keyboard Margins")]
     [SerializeField] private float topMargin;
@@ -30,7 +31,35 @@ public class Keyboard : MonoBehaviour
     void Start()
     {
         CreateKeys();
+=======
+    [Range(0f,1f)]
+    [SerializeField] private float keyboardWidth;
+    [Range(0f, 1f)]
+    [SerializeField] private float keyboardHeight;
+    [Header("Margin Settings")]
+    [Range(0f, 0.5f)]
+    [SerializeField] private float topMargin = 0.1f;     // Defaults to 10% of keyboard height
+    [Range(0f, 0.5f)]
+    [SerializeField] private float bottomMargin = 0.1f;  // Defaults to 10% of keyboard height
+    [Range(0f, 0.5f)]
+    [SerializeField] private float leftMargin = 0.1f;    // Defaults to 10% of keyboard width
+    [Range(0f, 0.5f)]
+    [SerializeField] private float rightMargin = 0.1f;   // Defaults to 10% of keyboard width
+
+    void Start()
+    {
+        rectTransform.sizeDelta = new Vector2(Screen.width / (1 / keyboardWidth), Screen.height / (1/keyboardHeight));
+        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, rectTransform.sizeDelta.y * 0.6f);
+
+        CreateKeys();
+    }
+
+    void Update()
+    {
+>>>>>>> parent of b337145 (Keyboard more functionality ++)
         PlaceKeys();
+        rectTransform.sizeDelta = new Vector2(Screen.width / (1 / keyboardWidth), Screen.height / (1 / keyboardHeight));
+        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, rectTransform.sizeDelta.y * 0.6f);
     }
 
     private void CreateKeys()
@@ -39,10 +68,14 @@ public class Keyboard : MonoBehaviour
         {
             for (int j = 0; j < lines[i].keys.Length; j++)
             {
-                string keyString = lines[i].keys[j];
+                string keyStr = lines[i].keys[j];
                 Keys keyInstance = Instantiate(keyPrefab, rectTransform);
+<<<<<<< HEAD
                 keyInstance.SetKey(keyString);
                 allKeys.Add(keyInstance);
+=======
+                keyInstance.SetKey(keyStr);
+>>>>>>> parent of b337145 (Keyboard more functionality ++)
             }
         }
     }
@@ -50,7 +83,15 @@ public class Keyboard : MonoBehaviour
     private void PlaceKeys()
     {
         int lineCount = lines.Length;
+<<<<<<< HEAD
         float keyWidth = (rectTransform.rect.width - leftMargin) * keyToLineRatio / lineCount;
+=======
+
+        float availableHeight = rectTransform.rect.height * (1 - topMargin - bottomMargin);
+        float availableWidth = rectTransform.rect.width * (1 - leftMargin - rightMargin);
+
+        float keyWidth = availableHeight * keyToLineRatio / lineCount;
+>>>>>>> parent of b337145 (Keyboard more functionality ++)
         float keyHeight = keyWidth;
         float lineHeight = keyHeight + (keyWidth * keyYSpacingRatio);
         float xSpacing = keyXSpacing * keyWidth;
@@ -60,23 +101,44 @@ public class Keyboard : MonoBehaviour
         {
             float halfKeyCount = (float)lines[i].keys.Length / 2;
             float totalLineWidth = (keyWidth + xSpacing) * lines[i].keys.Length - xSpacing;
+<<<<<<< HEAD
             float startX = -totalLineWidth / 2 + keyWidth / 2 + leftMargin;
             float lineY = rectTransform.rect.height / 2 - topMargin - (lineHeight / 2) - i * lineHeight;
+=======
+            float startX = -totalLineWidth / 2 + keyWidth / 2 + (rectTransform.rect.width - availableWidth) / 2;
+            float lineY = rectTransform.rect.height * (1 - topMargin) / 2 - (lineHeight / 2) - i * lineHeight;
+>>>>>>> parent of b337145 (Keyboard more functionality ++)
 
             for (int j = 0; j < lines[i].keys.Length; j++)
             {
                 float keyX = startX + j * (keyWidth + xSpacing);
+<<<<<<< HEAD
                 Vector2 keyAnchoredPosition = new Vector2(keyX, lineY);
                 RectTransform keyRectTransform = allKeys[currentKeyIndex].GetComponent<RectTransform>();
                 keyRectTransform.anchoredPosition = keyAnchoredPosition;
                 keyRectTransform.sizeDelta = new Vector2(keyWidth, keyWidth);
+=======
+
+                Vector2 keyAnchoredPosition = new Vector2(keyX, lineY);
+                RectTransform keyRectTransform = rectTransform.GetChild(currentKeyIndex).GetComponent<RectTransform>();
+                keyRectTransform.anchoredPosition = keyAnchoredPosition;
+                keyRectTransform.sizeDelta = new Vector2(keyWidth, keyHeight);
+
+>>>>>>> parent of b337145 (Keyboard more functionality ++)
                 currentKeyIndex++;
             }
         }
     }
+<<<<<<< HEAD
 }
 
 [System.Serializable]
+=======
+   }
+
+
+    [System.Serializable]
+>>>>>>> parent of b337145 (Keyboard more functionality ++)
 public struct KeyboardLine
 {
     public string[] keys;
--- Help, I Was Reincarnated as a Fruit and Now I Have to Participate in A Gameshow/Assets/Scripts/UI/Keyboard/Keys.cs
+++ Help, I Was Reincarnated as a Fruit and Now I Have to Participate in A Gameshow/Assets/Scripts/UI/Keyboard/Keys.cs
@@ -1,13 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
 public class Keys : MonoBehaviour
 {
+<<<<<<< HEAD
     [SerializeField] private TMP_Text keyText;
 
     public void SetKey(string key)
     {
         keyText.text = key;
+=======
+    [Header(" Elements ")]
+    [SerializeField] private TMP_Text keyText;
+    private string keyValue;  // Changed to string
+
+    public void SetKey(string key)
+    {
+        this.keyValue = key;
+        keyText.text = keyValue;
+
+        if (key == "CAPS")
+        {
+            keyText.fontSize = keyText.fontSize * 0.5f;
+        }
+        else
+        {
+            keyText.fontSize = keyText.fontSize * 1f;
+        }
+    }
+
+    public Button GetButton()
+    {
+        return GetComponent<Button>();
+>>>>>>> parent of b337145 (Keyboard more functionality ++)
     }
 }

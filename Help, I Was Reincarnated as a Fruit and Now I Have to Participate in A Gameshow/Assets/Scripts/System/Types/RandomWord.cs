1dc72ed5df0d858afaa1336967a79718cb13237d
Alfonso <dit19039@uop.gr>
2023-11-15 13:17:55 +0200

    animation

--- Help, I Was Reincarnated as a Fruit and Now I Have to Participate in A Gameshow/Assets/Scripts/System/Types/RandomWord.cs
+++ Help, I Was Reincarnated as a Fruit and Now I Have to Participate in A Gameshow/Assets/Scripts/System/Types/RandomWord.cs
@@ -1,25 +1,24 @@
 ﻿using UnityEngine;
-using TMPro; 
-using System.Collections;
-using System;
+using TMPro; // Make sure to include this for TextMeshPro
+using System; // For System.Random
 
 public class RandomWord : MonoBehaviour
 {
-    public TextMeshProUGUI wordText;
-    public TextMeshProUGUI categoryText;
-    public TextMeshProUGUI describeText;
-    public TextMeshProUGUI skipsText;
-    public TextMeshProUGUI timerText;
-    public TextMeshProUGUI gameOverText;
-    public TextMeshProUGUI instructionText; 
+    public TextMeshProUGUI wordText; // Assign this in the Unity Inspector
+    public TextMeshProUGUI categoryText; // Assign this in the Unity Inspector
+    public TextMeshProUGUI describeText; // Text for the description method
+    public TextMeshProUGUI skipsText; // Text for displaying remaining skips
+    public TextMeshProUGUI timerText; // Text for displaying the timer
+    public TextMeshProUGUI gameOverText; // Text for displaying "GAME OVER" and points
+    public TextMeshProUGUI instructionText; // Text for "Press X if you guessed right" instruction
 
     private WordGuessingGame guessingGame;
     private string[] descriptionMethods = new string[] { "sounds", "charade", "one word" };
     private System.Random random = new System.Random();
-    private int totalPoints = 0; 
-    private int skipCount = 0; 
-    private const int maxSkips = 3; 
-    private float timer = 30f;
+    private int totalPoints = 0; // Points accumulator
+    private int skipCount = 0; // Number of skips used
+    private const int maxSkips = 3; // Maximum number of skips allowed
+    private float timer = 30f; // 30 seconds timer
     private bool isGameOver = false;
 
     void Start()
@@ -27,31 +26,33 @@ public class RandomWord : MonoBehaviour
         guessingGame = new WordGuessingGame();
         DisplayRandomWord();
         UpdateSkipsText();
-        UpdateInstructionText(true);
-        gameOverText.gameObject.SetActive(false);
+        UpdateInstructionText(true); // Set the instruction text at the start
+        gameOverText.gameObject.SetActive(false); // Hide game over text initially
     }
 
     void Update()
     {
         if (!isGameOver)
         {
-  
+            // Update timer
             timer -= Time.deltaTime;
             UpdateTimerText();
 
+            // Check for game over
             if (timer <= 0)
             {
                 EndGame();
                 return;
             }
 
+            // Check for input to generate a new word
             if (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Return))
             {
                 DisplayRandomWord();
             }
 
             // Handling skip input
-            if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.Backspace)) && skipCount < maxSkips)
+            if ((Input.GetButtonDown("Fire2") || Input.GetKeyDown(KeyCode.Backspace)) && skipCount < maxSkips)
             {
                 skipCount++;
                 DisplayRandomWord();
@@ -62,61 +63,38 @@ public class RandomWord : MonoBehaviour
 
     void DisplayRandomWord()
     {
-        StartCoroutine(ChangeWordWithMovement());
-    }
-
-    IEnumerator ChangeWordWithMovement()
-    {
-        float moveTime = 0.5f;
-        Vector3 offScreenPosition = new Vector3(-6000, 8000, 0);
-        Vector3 wordPosition = new Vector3(-16.933f, 609f, 0f); // Updated position for word
-        Vector3 categoryPosition = new Vector3(-16.933f, 207.661f, 0f); // Updated position for category
-        Vector3 descriptionPosition = new Vector3(-16.933f, -163.536f, 0f); // Updated position for description
-
-        // Move the texts off-screen
-        StartCoroutine(MoveText(wordText, offScreenPosition, moveTime));
-        StartCoroutine(MoveText(categoryText, offScreenPosition, moveTime));
-        StartCoroutine(MoveText(describeText, offScreenPosition, moveTime));
-
-        yield return new WaitForSeconds(moveTime);
-
-        // Change the word
         GameWord randomWord = guessingGame.ChooseRandomWord();
-        wordText.text = "Word: " + randomWord.Word;
 
-        // Move the word text into view
-        yield return MoveText(wordText, wordPosition, moveTime);
+        if (wordText != null)
+            wordText.text = "Word: " + randomWord.Word;
+        else
+            Debug.LogError("wordText is not assigned in the Inspector");
 
-        // Update and move the category text into view
-        categoryText.text = "Category: " + randomWord.Category;
-        yield return MoveText(categoryText, categoryPosition, moveTime);
+        if (categoryText != null)
+            categoryText.text = "Category: " + randomWord.Category;
+        else
+            Debug.LogError("categoryText is not assigned in the Inspector");
 
-        // Update and move the description method text into view
-        describeText.text = "Describe using: " + GetRandomDescriptionMethod();
-        yield return MoveText(describeText, descriptionPosition, moveTime);
+        if (describeText != null)
+            describeText.text = "Describe using: " + GetRandomDescriptionMethod();
+        else
+            Debug.LogError("describeText is not assigned in the Inspector");
+
+        // Update points only if not skipping
+        if (skipCount <= maxSkips)
+            AccumulatePoints(randomWord.Points);
     }
 
-
-
-
-    IEnumerator MoveText(TextMeshProUGUI textElement, Vector3 targetPosition, float duration)
-    {
-        Vector3 startPosition = textElement.transform.localPosition;
-        float time = 0;
-
-        while (time < duration)
-        {
-            time += Time.deltaTime;
-            textElement.transform.localPosition = Vector3.Lerp(startPosition, targetPosition, time / duration);
-            yield return null;
-        }
-
-        textElement.transform.localPosition = targetPosition;
-    }
     string GetRandomDescriptionMethod()
     {
         int index = random.Next(descriptionMethods.Length);
         return descriptionMethods[index];
+    }
+
+    void AccumulatePoints(int points)
+    {
+        totalPoints += points;
+        // Points are now accumulated but not displayed
     }
 
     void UpdateSkipsText()
@@ -166,4 +144,3 @@ public class RandomWord : MonoBehaviour
             Debug.LogError("gameOverText is not assigned in the Inspector");
     }
 }
-
